7bdf7e64caabafd00f14c1cf73debeacc454c344
waliddeda2009-netizen <waliddeda2009@gmail.com>
2026-08-21 02:05:20 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,22 @@
-//==================================
-// FIREBASE
-//==================================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyDQYV1RhFh2_DcZd4SZmRxi49fTA9jBBfQ",
-
     authDomain: "crepe-station.firebaseapp.com",
-
     projectId: "crepe-station",
-
     storageBucket: "crepe-station.firebasestorage.app",
-
     messagingSenderId: "238191964316",
-
-    appId: "1:238191964316:web:1cc88bd70b4c9d3d0a7d59"
-
+    appId: "1:238191964316:web:1cc88bd70b4c9d3d0a7d59",
+    measurementId: "G-1KFESK66LN"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-
 const storage = getStorage(app);
+const analytics = getAnalytics(app);
 
-export { db, storage };
+export { db, storage, analytics };
